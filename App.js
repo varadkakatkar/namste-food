32fe873caf0ba873487143bf2a86dcd3732b6590
Varad Kakatkar <varadkakatkar@gmail.com>
2025-09-07 22:33:42 +0530

    wip food app

--- App.js
+++ App.js
@@ -1,41 +1,77 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
+import logo from "./logo.png";
 
-const Header = () =>{
-    return (
-        <div className="header">
-            <div className="logo-container">
-                <img 
-                className="logo"
-                src="logo.png"
-                />
-            </div>
-            <div className="nav-items">
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Contact Us</li>
-                <li>Cart</li>
-            </div>
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          src="https://cdn.jsdelivr.net/gh/varadkakatkar/namste-food@main/logo.png"
+          alt="Logo"
+          className="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/FOOD_CATALOG/IMAGES/CMS/2025/4/17/ef2f1053-c8e3-4072-aaa7-4539a67c8554_e8da5065-4d03-47d7-bad7-5413398a1564.jpg_compressed"
+      />
+      <h3>Meghana Foods</h3>
+      <h4>Biryanni, North Indian, Asian</h4>
+      <h4>4.4</h4>
+      <h4>38 mins</h4>
+    </div>
+  );
+};
 
-}
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search </div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard /> 
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard /> 
+        <RestaurantCard />
+        <RestaurantCard /> 
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard /> 
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
 
-
-const AppLayout = () =>{
-    return (
-        <div className="app">
-          {
-            // header 
-            <Header/>
-          }
-        </div>
-    )
-
-}
-
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout/>)
+root.render(<AppLayout />);
