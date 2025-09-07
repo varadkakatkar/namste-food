db5033ac45666c5091bd1edf5cfa8e64c59991f6
Varad Kakatkar <varadkakatkar@gmail.com>
2025-09-07 19:59:59 +0530

    second commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+
+const App = () =>{
+    return (
+        <div>
+           <h1> App component</h1>
+        </div>
+    )
+
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App/>)
