4c8ac742a140149792843ef10ab3a25adbf897b6
Varad Kakatkar <varadkakatkar@gmail.com>
2025-09-12 22:33:51 +0530

    react-router-dom in progress

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const About = () =>{
+    return(
+        <div>
+           <h1> About Us</h1>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/Error.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useRouteError } from 'react-router-dom'
+
+
+const Error = () => {
+    const err = useRouteError();
+    console.log(err);
+
+  return (
+    <div>
+        <h1>Opps!!</h1>
+        <h2>Something Went Wrong</h2>
+        <h2>{ err.status }:{err.statusText}</h2>
+    </div>
+  )
+}
+
+export default Error
